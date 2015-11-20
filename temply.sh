d3dc12ec74757670305166ddaa57092c5caddc75
Leonard Marschke <leonard@marschke.me>
2015-11-20 10:43:32 +0100

    fix variable replacement, introduce new syntax

--- temply.sh
+++ temply.sh
@@ -1,40 +1,32 @@
 #!/bin/bash
 
 function parseTemplate {
-	function existsKey {
-		if [ "$2" != in ]; then
-			echo "Incorrect usage."
-			echo "Correct usage: existsKey {key} in {array}"
-			return
-		fi
-		eval '[ ${'$3'[$1]} ]'
-	}
-
 	function replaceFiles {
 		echo "Replacing $1" >&2
 		local template=`cat $1`
 		local includePath=`dirname $1`
-		local re='(.*)!FILE<(.*)>(.*)'
+		local re='(.*)\{include=([[:alnum:]_-.]+)\}(.*)'
 		while [[ $template =~ $re ]]; do
+			echo "${BASH_REMATCH[2]}" >&2
+			echo "REMATCHEND" >&2
 			local rep=$(replaceFiles ${includePath}/${BASH_REMATCH[2]})
-			template="${BASH_REMATCH[1]}${rep}${BASH_REMATCH[3]}"
+			local template="${BASH_REMATCH[1]}${rep}${BASH_REMATCH[3]}"
 		done
 		echo "$template"
 	}
 
 	function replaceVars {
 		local template="$1"
-		local params="$2"
-		local re='(.*)!VAR<(.*)>(.*)'
+		local re='(.*)\{\$([[:alnum:]_-.]+)\}(.*)'
 		while [[ $template =~ $re ]]; do
 			local key="${BASH_REMATCH[2]}"
-			if existsKey $key in $params; then
-				local rep="${params[$key]}"
+			if [ ${vars[$key]+yes} ]; then
+				local rep="${vars[$key]}"
 			else
-				echo "WARNING: $key is not in your params array!" >&2
+				echo "WARNING: $key is not in your vars array!" >&2
 				local rep=""
 			fi
-			template="${BASH_REMATCH[1]}${rep}${BASH_REMATCH[3]}"
+			local template="${BASH_REMATCH[1]}${rep}${BASH_REMATCH[3]}"
 		done
 		echo "$template"
 	}
@@ -42,10 +34,13 @@ function parseTemplate {
 	local result=$(replaceFiles $1)
 
 	# do we got a variable array?
-	params="$2"
-	if [[ "$(declare -p params)" =~ "declare -A" ]]; then
-		replaceVars "${result//'/\'}" params
+	if [ "$2" != "" ]; then
+		declare -n vars=$2
+	else
+		echo "WARNING: No variables array passed!" >&2
+		declare -A vars
 	fi
+	result=$(replaceVars "${result}")
 
 	echo "$result"
 }
