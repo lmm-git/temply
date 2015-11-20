535cd594af7f32d4b73192f2f3711a3a83b44773
Leonard Marschke <leonard@marschke.me>
2015-11-20 12:38:41 +0100

    add if statements

--- temply.sh
+++ temply.sh
@@ -53,6 +53,78 @@ function temply {
 		echo "$template"
 	}
 
+	function handleIf {
+		local template="$1"
+		# build our regex to match variable tags
+		local startRe='(.*)\{if (((\$)?[[:alnum:]_.{}"]+) (==|!=|-gt|-lt|-le|-ge) ((\$)?[[:alnum:]_.{}"]+))\}(.*)'
+		local endRe='(.*)\{\/if\}(.*)'
+
+		# do for all variable tags
+		while [[ $template =~ $startRe ]]; do
+			# store BASH_REMATCH vars in local variables because we will override BASH_REMATCH before we need theese vars
+			local beforeRegex="${BASH_REMATCH[1]}"
+			local afterRegex="${BASH_REMATCH[8]}"
+			local op1="${BASH_REMATCH[3]}"
+			local op2="${BASH_REMATCH[6]}"
+			local comp="${BASH_REMATCH[5]}"
+			local restStr="${BASH_REMATCH[8]}"
+
+			# initialise some variable for the for loop
+			local openIf=1
+			local innerIf=''
+			local checkStr=''
+			# loop through each char in rest string
+			for (( i=0; i<${#restStr}; i++ )); do
+				local checkStr="${checkStr}${restStr:$i:1}"
+				local innerIf="${innerIf}${restStr:$i:1}"
+				if [[ "$checkStr" =~ $startRe ]]; then
+					# we found another if, so increase our open ifs
+					checkStr=''
+					let "openIf += 1"
+				elif [[ "$checkStr" =~ $endRe ]]; then
+					# we found a closing tag, so decrease our open ifs
+					checkStr=''
+					let "openIf -= 1"
+					# check if we have no open ifs left (we found the closing tag for our if)
+					if [ "$openIf" -le 0 ]; then
+						break;
+					fi
+				fi
+			done
+			# hmm if we get here the template is invalid
+			if [ "$openIf" -gt 0 ]; then
+				echo "Invalid template syntax! Check if you are closing all your if statements!" >&2
+				return
+			fi
+
+			# define the regex to match variables
+			local varRegex='\$([[:alnum:]_]+)'
+
+			# replace variables with them we got as argument
+			if [[ "$op1" =~ $varRegex ]]; then
+				op1="\"${vars[${BASH_REMATCH[1]}]}\""
+			fi
+			if [[ "$op2" =~ $varRegex ]]; then
+				op2="\"${vars[${BASH_REMATCH[1]}]}\""
+			fi
+
+			# do the comparsion of our if statement
+			if [ $op1 $comp $op2 ]; then
+				# add the text which stands before the regex,
+				# our text inside the if (minus 5 chars for our closing tag at the end) and
+				# all the text coming after the closing if tag
+				template="${beforeRegex}${innerIf::-5}${afterRegex:${#innerIf}}"
+			else
+				# add the text which stands before the regex,
+				# all the text coming after the closing if tag
+				template="${beforeRegex}${afterRegex:${#innerIf}}"
+			fi
+		done
+
+		# return our processed template
+		echo "$template"
+	}
+
 	local result=$(replaceFiles $1)
 
 	# do we got a variable array?
@@ -69,6 +141,9 @@ function temply {
 
 	# call the variable substitution function
 	result=$(replaceVars "${result}")
+
+	# process if statements
+	result=$(handleIf "${result}")
 
 	# return our final result
 	echo "$result"
